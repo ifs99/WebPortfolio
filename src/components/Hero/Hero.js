684d392a38add40a106e9ea17f5ad571770f790f
Italo Flores <ifloressar15>
2023-09-05 21:33:11 -0500

    Task:Hero View

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,10 +4,20 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+ <Section row nopadding>
+  <LeftSection>
+    <SectionTitle main center>
+      Welcome to <br />
+      My Personal Portfolio
+    </SectionTitle>
+    <SectionText>
+      Im currently looking to land a job in the tech industry, here you can 
+      find my projects and the technologies i used to develop them!
+    </SectionText>
+    <Button onClick={()=> window.location = 'https://google.com'}>Learn more</Button>
+  </LeftSection>
+</Section>
 );
 
 export default Hero;
